07ca6efd81ad58054a964deef4215c5c52e66334
andrzejwl <andrzejw239@gmail.com>
2021-10-31 12:11:55 +0100

    feat: fingerprint auth initial

--- rs_android/app/src/main/java/com/ksiezyk/roommanagementsystem/ui/login/LoginActivity.java
+++ rs_android/app/src/main/java/com/ksiezyk/roommanagementsystem/ui/login/LoginActivity.java
@@ -14,15 +14,21 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
 
 import com.ksiezyk.roommanagementsystem.ui.MainActivity;
 import com.ksiezyk.roommanagementsystem.R;
 import com.ksiezyk.roommanagementsystem.databinding.ActivityLoginBinding;
+
+import java.util.concurrent.Executor;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -43,6 +49,53 @@ public class LoginActivity extends AppCompatActivity {
         final EditText passwordEditText = binding.password;
         final Button loginButton = binding.login;
         final ProgressBar loadingProgressBar = binding.loading;
+
+        // fingerprint authentication
+        final Button biometricButton = binding.biometricBtn;
+        final TextView biometricText = binding.biomInfoText;
+        BiometricManager biometricManager = androidx.biometric.BiometricManager.from(this);
+
+        int canAuth = biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_WEAK);
+        switch (canAuth) {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                biometricText.setText("You can authenticate with fingerprint scanner.");
+//                biometricButton.setVisibility(View.VISIBLE);
+                break;
+            default:
+                biometricText.setText("Fingerprint scanner unavailable, error code: "+canAuth);
+//                biometricButton.setVisibility(View.GONE);
+                break;
+        }
+
+        Executor executor = ContextCompat.getMainExecutor(this);
+
+        final BiometricPrompt biometricPrompt = new BiometricPrompt(LoginActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
+
+            // on success
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                biometricText.setText("Fingerprint authentication successful!");
+            }
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                biometricText.setText("Fingerprint authentication failed.");
+            }
+        });
+
+        final BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder().setTitle("Fingerprint Authentication")
+                .setDescription("Use your fingerprint to login ").setNegativeButtonText("Cancel").build();
+        biometricButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                biometricPrompt.authenticate(promptInfo);
+            }
+        });
 
         loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
             @Override

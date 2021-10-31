08057bf03570f3c8de2f27087b080ae08dc48533
andrzejwl <andrzejw239@gmail.com>
2021-10-31 14:55:31 +0100

    refactor(biometrics): trigger user login

--- rs_android/app/src/main/java/com/ksiezyk/roommanagementsystem/ui/login/LoginActivity.java
+++ rs_android/app/src/main/java/com/ksiezyk/roommanagementsystem/ui/login/LoginActivity.java
@@ -59,43 +59,12 @@ public class LoginActivity extends AppCompatActivity {
         switch (canAuth) {
             case BiometricManager.BIOMETRIC_SUCCESS:
                 biometricText.setText("You can authenticate with fingerprint scanner.");
-//                biometricButton.setVisibility(View.VISIBLE);
                 break;
             default:
                 biometricText.setText("Fingerprint scanner unavailable, error code: "+canAuth);
-//                biometricButton.setVisibility(View.GONE);
+                biometricButton.setVisibility(View.GONE);
                 break;
         }
-
-        Executor executor = ContextCompat.getMainExecutor(this);
-
-        final BiometricPrompt biometricPrompt = new BiometricPrompt(LoginActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
-            @Override
-            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
-                super.onAuthenticationError(errorCode, errString);
-            }
-
-            // on success
-            @Override
-            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
-                super.onAuthenticationSucceeded(result);
-                biometricText.setText("Fingerprint authentication successful!");
-            }
-            @Override
-            public void onAuthenticationFailed() {
-                super.onAuthenticationFailed();
-                biometricText.setText("Fingerprint authentication failed.");
-            }
-        });
-
-        final BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder().setTitle("Fingerprint Authentication")
-                .setDescription("Use your fingerprint to login ").setNegativeButtonText("Cancel").build();
-        biometricButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                biometricPrompt.authenticate(promptInfo);
-            }
-        });
 
         loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
             @Override
@@ -130,6 +99,38 @@ public class LoginActivity extends AppCompatActivity {
 
                 //Complete and destroy login activity once successful
                 finish();
+            }
+        });
+
+        Executor executor = ContextCompat.getMainExecutor(this);
+
+        final BiometricPrompt biometricPrompt = new BiometricPrompt(LoginActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
+
+            // on success
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                biometricText.setText("Fingerprint authentication successful!");
+                loadingProgressBar.setVisibility(View.VISIBLE);
+                loginViewModel.login("user1@test.com", "password1");
+            }
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                biometricText.setText("Fingerprint authentication failed.");
+            }
+        });
+
+        final BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder().setTitle("Fingerprint Authentication")
+                .setDescription("Use your fingerprint to login ").setNegativeButtonText("Cancel").build();
+        biometricButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                biometricPrompt.authenticate(promptInfo);
             }
         });
 

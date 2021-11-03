dbc25ae121abc67d9a4a37a75058a4917a24d006
Adam Księżyk <adam.ksiezyk@gmial.com>
2021-11-03 01:24:53 +0100

    Refactor navbar to support NavigationUI

--- rs_android/app/src/main/java/com/ksiezyk/roommanagementsystem/ui/MainActivity.java
+++ rs_android/app/src/main/java/com/ksiezyk/roommanagementsystem/ui/MainActivity.java
@@ -2,12 +2,12 @@ package com.ksiezyk.roommanagementsystem.ui;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
@@ -15,36 +15,44 @@ import com.google.android.material.navigation.NavigationView;
 import com.ksiezyk.roommanagementsystem.R;
 
 public class MainActivity extends AppCompatActivity {
+    private final int[] fragmentIds = {
+            R.id.homeFragment,
+            R.id.reservationFragment,
+            R.id.logoutFragment,
+            R.id.settingsFragment
+    };
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Get UI elements
-        DrawerLayout drawerLayout = findViewById(R.id.main_drawer_layout);
-        Toolbar toolbar = findViewById(R.id.main_nav_toolbar);
-        NavigationView navigationView = findViewById(R.id.main_nav_view);
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.main_fragment_container_view);
-        NavController navController = navHostFragment.getNavController();
+        drawerLayout = findViewById(R.id.main_drawer_layout);
+        toolbar = findViewById(R.id.main_nav_toolbar);
+        navigationView = findViewById(R.id.main_nav_view);
 
-        // Setup toolbar and navigation menu
         setSupportActionBar(toolbar);
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
-                this,
-                drawerLayout,
-                toolbar,
-                R.string.nav_open,
-                R.string.nav_close
-        );
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
+    }
 
-        // Set Navigation as NavController for NavView
-        AppBarConfiguration appBarConfiguration =
-                new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupWithNavController(navigationView, navController);
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        navController = Navigation.findNavController(this, R.id.main_fragment_container_view);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(fragmentIds)
+                .setOpenableLayout(drawerLayout)
+                .build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, drawerLayout);
     }
 }
